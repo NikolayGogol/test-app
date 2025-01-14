7c6e8e134c52fe3799bc4d85438c3b0071b7245b
User <k.gogol13@gmail.com>
2025-01-14 16:52:02 +0200

    add header and footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="footer-wrapper">
+    <div class="container">
+      <div class="grid grid-cols-4 gap-[62px] items-center">
+        <div class="col-span-3 bg-[#0D0D0D] p-[33px] rounded-[8px] flex items-center justify-between">
+          <div class="block">
+            <div class="title">Tiebreaker Question</div>
+            <div class="sub-title">Which minute do you think the first goal will be scored in any of the six matches?
+            </div>
+          </div>
+          <div class="counter flex items-center">
+            <div class="controls">
+              <i class="fa-solid fa-chevron-up"></i>
+            </div>
+            <p>29</p>
+            <div class="controls">
+              <i class="fa-solid fa-chevron-down"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-span-1">
+          <div class="sub-title">After you submit your predictions, it can’t be undone</div>
+          <div class="grid grid-cols-2 gap-[13px] mt-[15px]">
+            <button class="cancel" type="button">Cancel</button>
+            <button class="submit" type="button">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {ref} from "vue";
+
+const timer = ref({
+  days: {
+    value: 5,
+  },
+  hours: {
+    value: 17,
+  },
+  minutes: {
+    value: 28,
+  },
+  seconds: {
+    value: 20
+  }
+})
+</script>
+
+<template>
+  <div class="container header-wrapper">
+    <div class="flex items-center justify-between pt-[53px]">
+      <div class="main-title">ROUND 18</div>
+      <div class="flex items-center">
+        <div class="block">
+          <div class="title">Countdown</div>
+          <div class="sub-title">Don’t miss!</div>
+        </div>
+        <div class="timer flex">
+          <div class="item" v-for="(item, key, index) in timer" :key="index">
+            <div class="value">{{ item.value }}</div>
+            <div class="name">{{ key }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
